794f8e22e6f1843a41e81cff964f6f498d5ed247
Jay Li <j_li@g.harvard.edu>
2020-05-09 13:21:01 -0400

    add current room type part

---
+++ conversion.py
@@ -0,0 +1,140 @@
+"""Classes for storing the raw inputs and svgs in structured ways
+"""
+import matplotlib.pyplot as plt
+from skimage.transform import resize
+import skimage
+import numpy as np
+import cairosvg
+from PIL import Image
+import io
+from lxml import etree
+
+from constants import INPUT_SHAPE, OUTPUT_SHAPE
+
+
+class FloorplanRaw:
+    """Class for raw floorplan image
+    """
+    def __init__(self, folder_path):
+        fpath = folder_path + "F1_scaled.png"
+        image =  plt.imread(fpath)
+        self.original_shape = image.shape[:2]
+        self.image = resize(image, INPUT_SHAPE)
+
+    def show(self):
+        plt.figure(figsize=(10, 10))
+        plt.imshow(self.image)
+
+
+def svgRead(filename):
+   """Load an SVG file and return image in Numpy array"""
+   # Make memory buffer
+   mem = io.BytesIO()
+   # Convert SVG to PNG in memory
+   cairosvg.svg2png(url=filename, write_to=mem)
+   # Convert PNG to Numpy array
+   return np.array(Image.open(mem))
+
+
+class Polygon:
+    def __init__(self, polygon_element, scaling_x, scaling_y):
+        points_str = polygon_element.get("points").split(" ")
+        points = []
+        for s in points_str:
+            if "," in s:
+                a, b = s.split(",")
+                points.append([int(float(a)*scaling_x),
+                               int(float(b)*scaling_y)])
+        self.points = points
+
+    @property
+    def x(self):
+        return [p[0] for p in self.points]
+
+    @property
+    def y(self):
+        return [p[1] for p in self.points]
+
+
+def read_room2class(fpath):
+    room2class = {}
+    with open(fpath, 'r') as f:
+        line = f.readline()
+        while line:
+            room_type, idx = line.split(":")
+            room2class[room_type] = int(idx)
+            line = f.readline()
+
+    return room2class
+
+
+def get_class2color(n, name='hsv'):
+    cmap = plt.cm.get_cmap(name, n)
+
+    class2color = {}
+    for idx in range(1, n):
+        class2color[idx] = cmap(idx)
+    return class2color
+
+
+class FloorplanSVG:
+    """Class for floorplan SVG file
+    """
+    def __init__(self, folder_path, original_shape, room2class):
+        self.original_shape = original_shape
+
+        scaling_x = OUTPUT_SHAPE[1]/original_shape[1]
+        scaling_y = OUTPUT_SHAPE[0]/original_shape[0]
+
+        # Extract rooms as polygons
+        rooms = []
+        fpath = folder_path + "model.svg"
+        tree = etree.parse(open(fpath, 'r'))
+        for element in tree.iter():
+            if self.get_tag(element) == "g" and self.is_room(element):
+                room_type = self.get_room_type(element)
+                polygon = Polygon(element.getchildren()[0],
+                                  scaling_x, scaling_y)
+                rooms.append([room_type, polygon])
+
+        # Create 2D array where each value is class id
+        semantic_map = np.zeros(dtype=int, shape=OUTPUT_SHAPE)
+        for room_type, polygon in rooms:
+            rr, cc = skimage.draw.polygon(polygon.y, polygon.x)
+            rr = np.clip(rr, 0, OUTPUT_SHAPE[0]-1)
+            cc = np.clip(cc, 0, OUTPUT_SHAPE[1]-1)
+            semantic_map[rr, cc] = room2class[room_type]
+        self.semantic_map = semantic_map
+
+        # image = svgRead(fpath)
+        # self.image = resize(image, (256, 256))
+
+    def show(self, base_image, class2color, class2room):
+        """Plot the class labelling by color for visualization
+        """
+        self.show_map(self.semantic_map, base_image, class2color, class2room)
+
+    @staticmethod
+    def show_map(semantic_map, base_image, class2color, class2room):
+        plt.figure(figsize=(10, 10))
+        # color_image = np.zeros(shape=(*self.semantic_map.shape, 3))
+        plt.imshow(base_image)
+        for cl in class2color:
+            coords = np.where(semantic_map == cl)
+            if len(coords[0]) == 0: continue
+            plt.plot(coords[1], coords[0], color=class2color[cl], alpha=0.5, label=class2room[cl])
+        plt.legend()
+
+    @staticmethod
+    def get_tag(element):
+        return element.tag.split("}")[1]
+
+    @staticmethod
+    def is_room(element):
+        cl = element.get("class")
+        return cl is not None and cl[:6] == "Space "
+
+    @staticmethod
+    def get_room_type(element):
+        return element.get("class")[6:]
+
---
+++ get_all_room_types.py
@@ -0,0 +1,37 @@
+from os import walk
+from lxml import etree
+from conversion import FloorplanSVG
+
+
+if __name__ == "__main__":
+    # Parse image directories
+    datadir = "./data/cubicasa5k/"
+    categories = ["colorful", "high_quality", "high_quality_architectural"]
+    image_dirs = {cat: None for cat in categories} # list of dir names in each category
+    for cat in categories:
+        path = datadir + cat
+        for _, dirnames, _ in walk(path):
+            image_dirs[cat] = dirnames
+            break
+
+    # Get all rooms types in dataset
+    room_types = set()
+
+    for cat, dirs in image_dirs.items():
+        for dir_ in dirs:
+            svg_path = datadir + cat + "/" + dir_ + "/model.svg"
+
+            # Parse room types in file
+            tree = etree.parse(open(svg_path, 'r'))
+            for element in tree.iter():
+                if FloorplanSVG.get_tag(element) == "g" and FloorplanSVG.is_room(element):
+                    room_type = FloorplanSVG.get_room_type(element)
+                    room_types.add(room_type)
+
+
+    # Save room types into a file as name to id correspondence
+    room_types = list(room_types)
+    fname = "room_types.txt"
+    with open(fname, "w") as f:
+        for idx, room_type in enumerate(room_types):
+            f.write(f"{room_type}: {idx+1}\n")
